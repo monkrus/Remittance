746e8e674e91b3897149ea6b471fa9ca48c826e5
Sergei Stadnik <sergeisqa@gmail.com>
2019-05-11 19:28:18 -0700

    Add mapped struct with index

---
+++ contracts/Remittance.sol
@@ -0,0 +1,41 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+//mapped struct with index
+contract Remittance is Ownable, Pausable {
+
+  struct Transaction {
+    uint transactionData;
+    bool isTransaction;
+  }
+
+  mapping(address => Transaction) public allTransactions;
+  address[] public transactionList;
+
+  function isTransaction(address transactionAddress) public view returns(bool isIndeed) {
+      return allTransactions[transactionAddress].isTransaction;
+  }
+
+  function getTransactionCount() public view returns(uint transactionCount) {
+    return transactionList.length;
+  }
+
+  function newTransaction(address transactionAddress, uint transactionData) public returns(uint rowNumber) {
+    if(isTransaction(transactionAddress)) revert("transaction not created");
+    allTransactions[transactionAddress].transactionData = transactionData;
+    allTransactions[transactionAddress].isTransaction = true;
+    return transactionList.push(transactionAddress) - 1;
+  }
+
+  function updateTransaction(address transactionAddress, uint transactionData) public returns(bool success) {
+    if(!isTransaction(transactionAddress)) revert("transaction not updated");
+    allTransactions[transactionAddress].transactionData = transactionData;
+    return true;
+  }
+}
+
